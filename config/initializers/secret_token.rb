07c3dab95480dea173366154cc6523f858c48ee8
nkj20932 <nkj20932@hotmail.com>
2014-04-14 23:32:04 +0800

    creating CRUD

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails1011::Application.config.secret_key_base = 'dd81816db14f9cad0add852858ab277170e32f3e44629737b0dc098d6d91ba4877c6166aa0649ddf729cc55f25ae9cb4e2dfb520061d524b9db89770b3ddd853'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rails1011::Application.config.session_store :cookie_store, key: '_rails101-1_session'
